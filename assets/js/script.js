fa5ca52342d659bfbafb7a384628576e7e481d3f
MarioRodrigo <mario.box@live.com>
2020-11-17 19:57:02 -0300

    avanzado 05 footer pendiete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
       $('[data-toggle="tooltip"]').tooltip()
     });
 
+    $(document).ready(function(){
+      $('[data-toggle="popover"]').popover();   
+    });
+
 });
