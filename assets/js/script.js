20e4c1fa575e9a7b8b9ecbb3e9726c2c926a156c
MarioRodrigo <mario.box@live.com>
2020-11-18 12:18:19 -0300

    avanzado 06 falta detalles y git-page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
       $('[data-toggle="tooltip"]').tooltip()
     });
 
-    $(document).ready(function(){
-      $('[data-toggle="popover"]').popover();   
+    $(function () {
+      $('[data-toggle="popover"]').popover()
     });
 
 });
